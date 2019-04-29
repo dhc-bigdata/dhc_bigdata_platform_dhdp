1f4d2117319ad6f1eb80b79d8c1bbd1c28da687f
wangwuwei <wangwuwei201314@163.com>
2019-04-29 16:53:55 +0800

    集成配置用户脚本以及配置初始化系统脚本

--- dhdp_bigdata_platform/bin/src/dhdp_each_host_install_tools.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_each_host_install_tools.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #安装基础软件、以及工具  比如pssh、java、mysql
 
+dhdp_bin_dir=/home/hadoop/dhdp/bin/
+
+hostname=`hostname`
+
 #只在最后一台机器安装
-dhdp_install_mysql.sh
+if [ $hostname == "hadoop03" ];then
+    cd $dhdp_bin_dir && bash dhdp_install_mysql.sh
+fi
