5278133a3770bc16df19d9102751faa3feaa9ad0
wangwuwei201314 <wangwuwei201314@163.com>
2019-05-06 17:11:46 +0800

    www文件格式转换2

--- dhdp_bigdata_platform/bin/src/dhdp_utils.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_utils.sh
@@ -1,30 +1,12 @@
 #! /bin/sh
 #文本及脚本文件格式如windows转Unix参考命令dos2unix
-dostype=dos
 #-e 转义反斜扛字符 -n 禁止换行 \b 删除前一个字符 \n 换行且光标移至行首
 IFS=$(echo -en "\n\b")
 function iterate_dir(){
-#	if [ "x$USER" != "xroot" ];then
-#		return
-#	fi
 	for file in $1/*; do
-	if [ -f $file ]; then
-		typename=`file $file | grep -q CRLF && echo dos || echo unix`
-		if [[ $typename == $dostype ]]; then
-			sed -i "s/.$//g" $file
-			echo 'converting '$file
-		else
-		:
-		fi
-	else
-		iterate_dir $file
-	fi
+		dos2unix $file
+		echo 'converting '$file
 	done
 	}
-if [ "$1" == "" ]; then
-	echo 'convering start'`pwd`
-	iterate_dir .
-else
-	echo 'convering start'$1
-	iterate_dir $1
-fi
+iterate_dir $1
+
