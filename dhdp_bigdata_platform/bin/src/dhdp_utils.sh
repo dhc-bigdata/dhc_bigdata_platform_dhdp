f9790b9744005bbb884df7115a6b6298fefda32e
刘锴 <lc00229397>
2019-05-07 14:02:32 +0800

    集成镜像挂载脚本 刘锴

--- dhdp_bigdata_platform/bin/src/dhdp_utils.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_utils.sh
@@ -9,4 +9,28 @@ function Doc2Unix(){
 	done
 	}
 Doc2Unix $1
+#挂载镜像
+function mount_os(){
+    mkdir mkdir
 
+    mount -o loop -t iso9660 CentOS-7-x86_64-Everything-1611.iso /media
+
+    jinrin n /etc/yum.repos.dwenjismk
+
+    rm  -f ./CentOS-Media.repo
+    touch  ./CentOS-Media.repo
+
+    echo "
+    [CentOS 7.3 1611]
+    #命名
+    name=CentOS-$releasever - Everything_ISO
+    #该镜像源已启用，若为0，则禁用
+    enabled=1
+    #镜像源地址
+    baseurl=file:///media/Packages/
+    #启用GPG校验，若为0，则禁用校验
+    gpgcheck=1
+    #校验文件（密钥）文件地址
+    gpgkey=file:///media/RPM-GPG-KEY-CentOS-7
+    " >> CentOS-Media.repo
+}
