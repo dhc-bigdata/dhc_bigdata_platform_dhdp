38050333cef4a81c6bcb9427501677bf8ebe463a
刘锴 <lc00229397>
2019-05-07 17:29:22 +0800

    增加安装时间记录 刘锴

--- dhdp_bigdata_platform/bin/src/dhdp_install_inner.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_install_inner.sh
@@ -15,3 +15,16 @@ if [ "x$1" != "xinstall" ];then
 	usage
 	exit 1
 fi
+#开始时间
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+
+dhdp_home=/home/hadoop/dhdp
+#安装pssh以简化此后操作
+bash $dhdp_home/bin/src/dhdp_install_pssh.sh
+
+
+#结束时间
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+end_seconds=$(date --date="$endtime" +%s);
+echo "本次运行时间： "$((end_seconds-start_seconds))"s"
