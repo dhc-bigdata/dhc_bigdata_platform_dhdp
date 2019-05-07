22ac44fdee3784d26ac5abd3f49621beb1a0988c
刘锴 <lc00229397>
2019-05-07 17:25:15 +0800

    增加挂载镜像

--- dhdp_bigdata_platform/bin/src/dhdp_utils.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_utils.sh
@@ -7,10 +7,10 @@ function Doc2Unix(){
 		dos2unix $file
 		echo 'converting '$file
 	done
-	}
-	if [ $hostname == "hadoop01" ];then
-            sync_time
-    fi
+}
+Doc2Unix $1
+
+#时间同步	
 function sync_time(){
     echo "sync_time begin......"
     rm -rf /etc/ntp.conf
@@ -21,5 +21,44 @@ function sync_time(){
     systemctl enable ntpd.service
     echo "sync_time end......"
 }
-Doc2Unix $1
+
+#挂载镜像
+function mount_os(){
+
+	mount -o loop -t iso9660 /root/dhdp/CentOS-7-x86_64-DVD-1810.iso /media
+	
+	mkdir -p /etc/yum.repos.d/bak
+	cd /etc/yum.repos.d/
+	cp *.repo ./bak
+	rm  -f ./CentOS-Media.repo
+	touch  ./CentOS-Media.repo
+	
+	echo "
+	[CentOS 7.3 1611]
+	#命名
+	name=CentOS-$releasever - Everything_ISO
+	#该镜像源已启用，若为0，则禁用
+	enabled=1
+	#镜像源地址
+	baseurl=file:///media/Packages/
+	#启用GPG校验，若为0，则禁用校验
+	gpgcheck=1
+	#校验文件（密钥）文件地址
+	gpgkey=file:///media/RPM-GPG-KEY-CentOS-7
+	" >> CentOS-Media.repo
+	
+	sed -i '/enabled=0/i\name=' CentOS-Base.repo
+	
+	yum clean all
+	
+	yum makecache
+	
+	yum repolist all
+
+}
+if [ $hostname == "hadoop01" ];then
+	sync_time
+else
+   echo "0-59/1 * * * * /usr/sbin/ntpdate hadoop01" > crontab
+fi
 
