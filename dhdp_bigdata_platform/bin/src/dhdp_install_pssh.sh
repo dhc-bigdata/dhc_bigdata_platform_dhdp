f5276690eacd8770e6d39e5ab4ffc6b4e7d82324
kai2liu <18302914430@163.com>
2019-05-10 11:01:42 +0800

    pssh地址

--- dhdp_bigdata_platform/bin/src/dhdp_install_pssh.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_install_pssh.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-#############################################################################
-#程序名:一键安装pssh
-#功能描述:一键安装pssh
-#作者:   liukai
-#创建日期: 20190424
-#输入:     日期 v_data_DATE              ($1)
-#输出: res 
-#系统变量： 执行上一个指令的返回值     ($?)
-#############################################################################
-#############################################################################
-#  修改者:
-#  修改日期:
-#  修改内容:
-##############################################################################
 ##下载上传pssh到/usr/local/pssh目录
 mkdir /usr/local/pssh
-pssh_install_dir=/home/hadoop/tools/package/
+dhdp_package_dir=/home/hadoop/dhdp/tools #安装包存放目录
 echo 'execute dhdp_install_pssh.sh start ...'
 ## 将安装包解压到安装位置，不输出日志
-cd /home/hadoop/tools/package/
-tar -xzvf /home/hadoop/tools/package/pssh-1.4.1.tar.gz
+cd /home/hadoop/dhdp/tools/
+tar -xzvf /home/hadoop/dhdp/tools/pssh-1.4.1.tar.gz
 ##上传setuptools-0.6c11.tar.gz 到/usr/local/pssh/pssh-1.4.1 并解压
-tar -xzvf /home/hadoop/tools/package/setuptools-0.6c11.tar.gz
-cd /home/hadoop/tools/package/setuptools-0.6c11
+tar -xzvf /home/hadoop/dhdp/tools/setuptools-0.6c11.tar.gz
+cd /home/hadoop/dhdp/tools/setuptools-0.6c11
 python setup.py build
 python setup.py install
 ##进入pssh目录
