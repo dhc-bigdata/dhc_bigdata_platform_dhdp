db2e96ec9029798a5af6a3972bac8ce4e8b6e91b
刘锴 <lc00229397@sina.com>
2019-05-14 21:23:36 +0800

    刘锴 修改格式

--- dhdp_bigdata_platform/bin/src/dhdp_init_hostname.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_init_hostname.sh
@@ -9,40 +9,37 @@ hpid2=0
 addr=192.168.120.11
 arid=4
 function init_hostname(){
-        for host in $hosts;
-                        do
-                        ((hpid1++))
-                        ((arid++))
-                                        echo "$addr$arid  $hostname$hpid1" >> /etc/hosts
-                        done
-        for ip in $ips;
-                do
-                        /usr/bin/expect <<-EOF
-                        set timeout 300
-                                spawn scp /etc/hosts root@$ip:/etc
-                        expect {
-#                                *(yes/no)* {send -- yes\r;exp_continue;}
-                                  *password:* {send -- root\r;exp_continue;}
-                        }
-						EOF
-                done
+	for host in $hosts;do
+		((hpid1++))
+		((arid++))
+		echo "$addr$arid  $hostname$hpid1" >> /etc/hosts
+		done
+	for ip in $ips;do
+		/usr/bin/expect <<-EOF
+		set timeout 300
+				spawn scp /etc/hosts root@$ip:/etc
+		expect {
+				*(yes/no)* {send -- yes\r;exp_continue;}
+				*password:* {send -- root\r;exp_continue;}
+		}
+		EOF
+    done
 }
-        function init_host(){
-        for ip in $ips;
-                do
-                        ((hpid2++))
-                        /usr/bin/expect <<-EOF
-                                        set timeout 300
-                                        spawn ssh -o stricthostkeychecking=no root@$ip
-                        expect {
-        #                               "(yes/no)" {send "yes\r"; exp_continue}
-                                                                        "password:" {send "root\n; exp_continue"}
-                        }
-                        expect "]#"  {send "echo '$hostname$hpid2'  >> /etc/hostname \n"}
-                        expect "]#"  {send "exit\n"}
-                        #expect eof
-						EOF
-                done
+function init_host(){
+	for ip in $ips;do
+		((hpid2++))
+		/usr/bin/expect <<-EOF
+		set timeout 300
+		pawn ssh -o stricthostkeychecking=no root@$ip
+		expect {
+	#           "(yes/no)" {send "yes\r"; exp_continue}
+				"password:" {send "root\n; exp_continue"}
+		}
+		expect "]#"  {send "echo '$hostname$hpid2'  >> /etc/hostname \n"}
+		expect "]#"  {send "exit\n"}
+		#expect eof
+		EOF
+	done
 }
 init_hostname
 init_host
