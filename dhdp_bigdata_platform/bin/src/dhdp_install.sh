3e8ad161e8b8568f1b33f7f82f87cc8f57387762
刘锴 <lc00229397@sina.com>
2019-05-14 21:58:14 +0800

    刘锴 程序入口安装dos2unix；并增加将dhdp_utils.sh提前解压出来；并转换解压出文件的格式

--- dhdp_bigdata_platform/bin/src/dhdp_install.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_install.sh
@@ -26,7 +26,12 @@ fi
 version=$1
 
 echo "uncompress dhdp_install_launch.sh ..."
-cd /root/dhdp && tar -zxf dhdp-$version.tar.gz dhdp-$version/bin/src/dhdp_install_launch.sh
+#安装dos2unix以便后续处理安装脚本格式问题
+yum install -y dos2unix
+cd /root/dhdp && tar -zxf dhdp-$version.tar.gz dhdp-$version/bin/src/dhdp_install_launch.sh dhdp-$version/bin/src/dhdp_utils.sh
+#处理文件格式
+dos2unix dhdp-$version/bin/src/dhdp_utils.sh
+dos2unix dhdp-$version/bin/src/dhdp_install_launch.sh
 
 echo "bash dhdp_install_launch.sh. show log in /root/logs/install_log_all.log ..."
 mkdir -p /root/logs
