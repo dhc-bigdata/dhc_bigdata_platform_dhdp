9087018880b0ef059c43b17cf5f58f44ca3ef2b7
ciyu001 <wlx1140248190@163.com>
2019-05-14 18:00:28 +0800

    修改ssh

--- dhdp_bigdata_platform/bin/src/dhdp_ssh_non_pwd_login.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_ssh_non_pwd_login.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-SERVERS="hadoop01  hadoop02  hadoop03"
-#password=root
-rm -rf /root/.ssh
+#hosts=hosts=`python dhdp_hadoop_xml_utils.py hostname`
+#ips=`python dhdp_hadoop_xml_utils.py IPs`
+ips="192.168.120.115 192.168.120.116"
+hosts="hadoop01 hadoop02"
+ssh_three
+function ssh_three(){
+for ip in $ips;
+                do
+                        /usr/bin/expect <<-EOF
+                                        set timeout 300
+                                        spawn ssh -o stricthostkeychecking=no root@$ip
+                        expect {
+        #                               "(yes/no)" {send "yes\r"; exp_continue}
+                                          password:" {send "root\n; exp_continue"}
+                        }
+                        expect "]#"  {send "rm -rf /root/.ssh \n"}
+                        expect "]#"  {send ". dhdp_utils.sh && ssh_key_gen \n"}
+		                expect "]#"  {send ". dhdp_utils.sh && ssh_copy_id_all \n"}
+                        expect "]#"  {send "exit\n"}
+                        #expect eof
+						EOF
+                done
+}
 function ssh_key_gen(){
 		/usr/bin/expect <<-EOF
 		set timeout 300
@@ -25,10 +45,8 @@ function ssh_copy_id(){
 		EOF
 }
 function ssh_copy_id_all(){
-		for SERVER in $SERVERS
+		for host in $hosts
 		do
-			ssh_copy_id $SERVER
+			ssh_copy_id $host
 		done
 }
-ssh_key_gen
-ssh_copy_id_all
