2b8cf9844ab571a7f6022be19e61f35609cffaf7
ciyu001 <wlx1140248190@163.com>
2019-05-14 16:36:11 +0800

    修改hostname

--- dhdp_bigdata_platform/bin/src/dhdp_init_hostname.sh
+++ dhdp_bigdata_platform/bin/src/dhdp_init_hostname.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
-hostnames="hadoop01 hadoop02 hadoop03"#hostname
-ips="192.168.120.115  192.168.120.116" #主机
+hosts=`python dhdp_hadoop_xml_utils.py hostname`
+ips=`python dhdp_hadoop_xml_utils.py IPs`
+#ips="192.168.120.115 192.168.120.116"
+#hosts="hadoop01 hadoop02"
+hostname=hadoop0
+hpid1=0
+hpid2=0
+addr=192.168.120.11
+arid=4
 function init_hostname(){
-	echo "$1"  >> /etc/hostname
-	for host in $hostnames and ip in $ips
-	do
-		echo "$host  $ip"  >> /etc/hosts
-	done
+        for host in $hosts;
+                        do
+                        ((hpid1++))
+                        ((arid++))
+                                        echo "$addr$arid  $hostname$hpid1" >> /etc/hosts
+                        done
+        for ip in $ips;
+                do
+                        /usr/bin/expect <<-EOF
+                        set timeout 300
+                                spawn scp /etc/hosts root@$ip:/etc
+                        expect {
+#                                *(yes/no)* {send -- yes\r;exp_continue;}
+                                  *password:* {send -- root\r;exp_continue;}
+                        }
+						EOF
+                done
 }
-init_hostname $1
-
-/usr/bin/expect <<EOF
-	set timeout 120
-	spawn ssh -o stricthostkeychecking=no root@${deploy_ip}
-	expect {
-			"(yes/no)" {send "yes\r"; exp_continue}
-			"password:" {send "${root_password}\n; exp_continue"}
-	}
-	expect "]#"  {send "mkdir -p ${target_to_path}/$time$dir_name \n"}
-	expect "]#"  {send "mkdir -p ${boot_scrpt} \n"}
-	expect "]#"  {send "exit\n"}
-	#expect eof
-EOF
+        function init_host(){
+        for ip in $ips;
+                do
+                        ((hpid2++))
+                        /usr/bin/expect <<-EOF
+                                        set timeout 300
+                                        spawn ssh -o stricthostkeychecking=no root@$ip
+                        expect {
+        #                               "(yes/no)" {send "yes\r"; exp_continue}
+                                                                        "password:" {send "root\n; exp_continue"}
+                        }
+                        expect "]#"  {send "echo '$hostname$hpid2'  >> /etc/hostname \n"}
+                        expect "]#"  {send "exit\n"}
+                        #expect eof
+						EOF
+                done
+}
+init_hostname
+init_host
